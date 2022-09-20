d2fb7e5d15c40c0a20eb80ae33964eb40a9af56d
nmelnik <nick.melnik@gmail.com>
2022-09-20 23:44:02 +1000

    removed old splash image

--- XRNeckSaferApp/Properties/Resources.Designer.cs
+++ XRNeckSaferApp/Properties/Resources.Designer.cs
@@ -71,16 +71,6 @@ namespace XRNeckSaferApp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap plane {
-            get {
-                object obj = ResourceManager.GetObject("plane", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon XRNS {
